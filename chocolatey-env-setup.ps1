2a6bd9765ad4a3c82fc607cf9eb3872dfccd7f36
atrantan <atrantan@gmail.com>
2020-06-03 19:37:13 +0200

    fix script

--- chocolatey-env-setup.ps1
+++ chocolatey-env-setup.ps1
@@ -7,7 +7,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 choco install -y ninja
 choco install -y cmake
 choco install -y mingw
-choco install -y gcc-arm
+choco install -y gcc-arm-embedded
 choco install -y openocd
 choco install -y git
 choco install -y vscode
